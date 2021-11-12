931c052a875da86b86b9e28803060060d9d8d930
eriklemy <eriklemy2010@gmail.com>
2021-11-12 12:18:11 -0300

    Update

--- js/game.js
+++ js/game.js
@@ -114,3 +114,35 @@ window.addEventListener('DOMContentLoaded', () => {
 
     resetButton.addEventListener('click', resetBoard);
 });
+
+// darkmode
+let darkMode = window.localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+if(darkMode === 'enabled'){
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const enableDarkMode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null);
+};
+
+if(darkMode === 'enabled') {
+    enableDarkMode();
+};
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if(darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
